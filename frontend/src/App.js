bc74439ee7b9c6380951c5a15e51621f370a17c2
jakob-bt <jakob.bergthomsen@gmail.com>
2023-05-08 00:49:37 +0200

    basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,37 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+import {Card, Button, Breadcrumb, BreadcrumbItem} from 'react-bootstrap'
 
-export default App
+function App() {
+
+  return (
+    <div>
+      <Card style={{ display: 'inline-block', width: '60%' }}>
+
+        <Card.Title>Test Test</Card.Title>
+        <Card.Text>0 Tweets</Card.Text>
+
+        <Card.Img src="https://picsum.photos/200/100"></Card.Img>
+        <Card.Img style={{ position: 'relative', bottom: '6em', width: '25%', height: '10em' }} className="rounded-circle" src="https://picsum.photos/100/200"></Card.Img>
+        <Card.Title style={{ position: 'relative', bottom: '4em', left: '3em' }} >Test Test</Card.Title>
+        <Card.Text style={{ position: 'relative', bottom: '5em', left: '4em' }}>@test</Card.Text>
+
+        <Button style={{ position: 'relative', bottom: '10em', left: '40em', width: '10%' }} >Follow</Button>
+
+        <div className="d-flex justify-content-evenly" style={{ position: 'relative', bottom: '3em'}}>
+        <Card.Text>Golf</Card.Text>
+        <Card.Text>New Mexico</Card.Text>
+        <Card.Text>example.com</Card.Text>
+        <Card.Text>Joined May 2023</Card.Text>
+        </div>
+        
+      </Card>
+
+      <Card style={{ position: 'relative', bottom: '32em', display: 'inline-block', width: '40%' }} >
+      <Card.Title>Trends for you</Card.Title>
+      <Card.Text>#This</Card.Text>
+      <Card.Text>#That</Card.Text>
+      </Card>
+      </div>
+  );
+}
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import './App.css';
 
-import App from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <App />
+);
